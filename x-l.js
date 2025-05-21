35dba26a4819b6968ffeda2cad50b15ec19dd7da
Joran <72360650+JohanAppeltaart@users.noreply.github.com>
2025-05-21 16:36:16 +0200

    epic

--- x-l.js
+++ x-l.js
@@ -9,60 +9,72 @@ const matrix = [
 ];
 
 function fromDataMakeTable(tbody) {
-  let dataMatrix = JSON.parse(localStorage.getItem("storedTable"))
+  let dataMatrix = JSON.parse(localStorage.getItem("storedTable"));
   if (!dataMatrix) {
-    dataMatrix = [["Name"],["peter"]]
+    dataMatrix = [["Name"], ["peter"]];
   }
   // console.log(dataMatrix)
-  let headerRow = dataMatrix.shift()
-  makeColumn (tbody, "")
+  let headerRow = dataMatrix.shift();
+  makeColumn(tbody, "");
   for (let peter = 0; peter < headerRow.length; peter++) {
     const value = headerRow[peter];
-    makeColumn (tbody, value)
+    makeColumn(tbody, value);
   }
-  dataMatrix.forEach(row => {
-    makeRow(tbody,row)
+  dataMatrix.forEach((row) => {
+    makeRow(tbody, row);
   });
 }
 
-function saveTableInMatrix(){
-  const table = document.querySelector(".table")
-  const tempMatrix = []
+function saveTableInMatrix() {
+  const table = document.querySelector(".table");
+  const tempMatrix = [];
   // header
-  let headerArr = []
-  let theadChilds = table.children[0].children[0].children
+  let headerArr = [];
+  let theadChilds = table.children[0].children[0].children;
   for (let i = 1; i < theadChilds.length; i++) {
     const head = theadChilds[i];
-    headerArr.push(head.innerText)
+    headerArr.push(head.innerText);
   }
   tempMatrix.push(headerArr);
-  
 
   // rows
-  let tbodyRows = table.children[1].children
+  let tbodyRows = table.children[1].children;
   for (let u = 0; u < tbodyRows.length; u++) {
     const row = tbodyRows[u].children;
-    let row1 = []
+    let row1 = [];
     for (let j = 1; j < row.length; j++) {
       const rowX = row[j];
-      row1.push(rowX.innerText)
+      row1.push(rowX.innerText);
     }
-    tempMatrix.push(row1)
+    tempMatrix.push(row1);
   }
 
-  localStorage.setItem("storedTable", JSON.stringify(tempMatrix))
+  localStorage.setItem("storedTable", JSON.stringify(tempMatrix));
+}
+
+function handleNetworkChange(header) {
+  if (navigator.onLine) {
+    header.innerText = "MegaSoft X-L";
+    header.style.backgroundColor = "#007BFF";
+    document.body.style.backgroundColor = "lavenderblush";
+  } else {
+    header.style.backgroundColor = "red";
+    header.innerText = "Offline MegaSoft X-L";
+    document.body.style.backgroundColor = "red";
+  }
 }
 
 function start() {
-  if ('serviceWorker' in navigator) {
+  if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-    .register("./serviceworker.js")
-    .then(()=>console.log("SW Registered."))
-    .catch((err)=> console.log(`error ${err}`))
- }
- 
+      .register("./serviceworker.js")
+      .then(() => console.log("SW Registered."))
+      .catch((err) => console.log(`error ${err}`));
+  }
+  const header = makeDom(document.body, "header", "MegaSoft X-L");
 
-  makeDom(document.body, "header", "MegaSoft X-L");
+  window.addEventListener("online", handleNetworkChange(header));
+  window.addEventListener("offline", handleNetworkChange(header));
 
   const main = makeDom(document.body, "main");
 
@@ -71,15 +83,14 @@ function start() {
   const saveTable = makeDom(buttonsDiv, "button", "resetTable");
   saveTable.onclick = () => {
     // localStorage.setItem("storedTable", JSON.stringify(matrix))
-    localStorage.clear()
-    location.reload()
+    localStorage.clear();
+    location.reload();
   };
 
   const columnButton = makeDom(buttonsDiv, "button", "Add column");
   columnButton.onclick = () => {
-    makeColumn (tbody, prompt("What do you want to name the column?"));
+    makeColumn(tbody, prompt("What do you want to name the column?"));
   };
-
 
   const rowButton = makeDom(buttonsDiv, "button", "Add row");
   rowButton.onclick = () => {
@@ -95,12 +106,11 @@ function start() {
 
   const tr1 = makeDom(thead, "tr", null, "headRow");
 
-  fromDataMakeTable(tbody)
+  fromDataMakeTable(tbody);
 }
 
 function makeRow(tbody, columnArray) {
-
-  const columnAmount = document.querySelectorAll("th").length
+  const columnAmount = document.querySelectorAll("th").length;
 
   const newRow = makeDom(tbody, "tr");
 
@@ -110,12 +120,11 @@ function makeRow(tbody, columnArray) {
     tbody.removeChild(newRow);
   });
 
-  for (let i = 0; i < columnAmount-1; i++) {
+  for (let i = 0; i < columnAmount - 1; i++) {
     if (columnArray != undefined) {
       makeCell(newRow, columnArray[i]);
-    }
-    else{
-      makeCell(newRow, "wow")
+    } else {
+      makeCell(newRow, "wow");
     }
   }
 
@@ -124,9 +133,9 @@ function makeRow(tbody, columnArray) {
 
 function makeCell(parent, value, classNameForDom, theader) {
   let cell;
-  if(theader){
+  if (theader) {
     cell = makeDom(parent, "th", value, classNameForDom);
-  }else{
+  } else {
     cell = makeDom(parent, "td", value, classNameForDom);
   }
 
@@ -171,15 +180,16 @@ function makeDom(parent, name, text, className) {
   return element;
 }
 
-
-function makeColumn (tbody, name) {
-    const headRow = document.querySelector(".headRow")
-    makeCell(headRow,name,null, true)
-    for (let x = 0; x < tbody.children.length; x++) {
-      const row = tbody.children[x];
-      makeCell(row, "");
-    }
-    saveTableInMatrix();
+function makeColumn(tbody, name) {
+  const headRow = document.querySelector(".headRow");
+  makeCell(headRow, name, null, true);
+  for (let x = 0; x < tbody.children.length; x++) {
+    const row = tbody.children[x];
+    makeCell(row, "");
+  }
+  saveTableInMatrix();
 }
 
 window.addEventListener("load", start);
+
+window.addEventListener("load", () => {});
